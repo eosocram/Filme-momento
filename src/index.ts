b20ab266813dfac2115bfb4f119b59e3cf193de4
Eosocram <122424153+eosocram@users.noreply.github.com>
2024-09-09 21:43:30 +0000

    Configura variáveis de ambiente e atualiza o código para usar a chave API do .env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-import express from 'express'; 
+import express from 'express';
+import axios from 'axios';
 
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
+const apiKey = process.env.API_KEY;
 
+app.get('/movies', async (req, res) => {
+    try {
+        console.log("Buscando filmes");
+        const response = await axios.get('https://api.themoviedb.org/3/movie/now_playing', { 
+            params: {
+                api_key: apiKey,
+                language: 'pt-BR',
+                page: 1
+            } 
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error("Erro ao buscar filmes...", error);
+        res.status(500).json({ error: 'Erro ao buscar os filmes' });
+    }
+});
 
 app.listen(port, () => {
-    console.log(`http:/localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
